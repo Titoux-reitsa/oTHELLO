c30f8e02209f9e4e2d8bf4fa4217db387ba8b2a0
Titoux-reitsa <mertrier5555@gmail.com>
2024-11-18 00:10:52 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -4,8 +4,10 @@
 
 // Fonctions de fonctions_bases.c
 
-void Init_tab(char plateau[Lenght_tab][Lenght_tab]);
-void Print_tab(char plateau[Lenght_tab][Lenght_tab]); //
-bool Is_empty(char plateau[Lenght_tab][Lenght_tab], int ligne, int column); // 
+void Init_tab(int plateau[Lenght_tab][Lenght_tab]);
+void Print_tab(int plateau[Lenght_tab][Lenght_tab]); 
+bool Is_empty(int plateau[Lenght_tab][Lenght_tab], int ligne, int column); 
+bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]);
+void swap(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]);
 
 // Fonctions de IA.c
