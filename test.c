48a8fc6a830887e6572850d4d8130107605d032d
Titoux-reitsa <mertrier5555@gmail.com>
2024-11-18 10:13:41 +0100

    Update test.c

--- test.c
+++ test.c
@@ -3,25 +3,13 @@
 #include <string.h>
 #include "fonctions.h"
 
-bool test(int plateau_entre[8][8],int plateau_sortie[8][8],int ligne, int colonne, int Indice_player,int Possible_vect[8][2]){
-    if(Is_possible(plateau_entre,ligne,colonne, Indice_player, Possible_vect)){
-        swap(plateau_entre,ligne,colonne, Indice_player, Possible_vect);
-        int cmpt =0;
-        for(int i=0 ; i < 8 ; i++){
-            for(int j=0 ; j < 8 ; j++){
-                if((plateau_entre[i][j]  == plateau_sortie[i][j])){
-                    cmpt++ ;
-                    if (cmpt == 64 ) {
-                        return 1; 
-                        printf ("test valide");
-                    }
-                    
-                }
-                else break;
-            }
-        }
-       
-    }
-    printf("les deux tableaux ne correspondent pas");
-    return 0;
-}
+int Possible_vect[8][2]={{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0},{0,0}};
+	if(coup==64){
+		return 0;
+	}
+	int j = coup%8;
+	int i = coup/8;
+	bool played = Is_possible(tableau,i,j,couleur,Possible_vect);
+	if(played)swap(tableau,i,j,couleur,Possible_vect);
+
+	return 1;
