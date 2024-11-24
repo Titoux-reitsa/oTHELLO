375236f81d3bef4b8f1bda2281d402f837c51562
Titoux-reitsa <mertrier5555@gmail.com>
2024-11-24 18:08:40 +0100

    Update main.c
    ajout d'une possibilité de passer son coup en rentrant -1 -1

--- main.c
+++ main.c
@@ -1,15 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "fonctions.h"
 
 int main(){
 
-    // Initialisation du tableau
-    char plateau[Lenght_tab][Lenght_tab];
+    // Initialisation du plateau
+    int plateau[Lenght_tab][Lenght_tab];
 
-    // Initialisation du tableau et affichage
+    // Initialisation du plateau et affichage
     Init_tab(plateau);
     Print_tab(plateau);
+
+    // Boucle de jeux
+
+    char tab_player[2] = {'N', 'B'};
+    int Indice_tab_player = 0;
+
+    int end_game = 0;
+
+    bool boucle = true;
+    while(boucle){
+        
+        int ligne;
+        int column;
+
+        int Possible_vect[8][2];
+        bool played = false;
+
+        if (end_game == 2){
+            int WW = Who_win(plateau);
+            if (WW != -1) printf("Le joueur %c a gagné ! \n",tab_player[WW]);
+            else printf("Egalié \n");
+            boucle = false;
+        }
+        else if (Tab_is_empty(plateau, Indice_tab_player, Possible_vect)){
+            played = true;
+            end_game += 1;
+        }
+        else {
+            
+            printf("\n");
+            printf("C'est au tour des pions %c\n", tab_player[Indice_tab_player]);
+            printf("Rentrer ligne : ");
+            scanf("%d", &ligne); // Rentrer ligne : 
+            printf("Rentrer colone : ");
+            scanf("%d", &column); // Rentrer colone : 
+            printf("\n");
+
+
+            played = Is_possible(plateau, ligne, column, Indice_tab_player, Possible_vect);
+            //condition pour passer son tour si le joueur rentre -1 -1 en ligne colonne
+            if(ligne = -1 && colonne = -1){
+                Indice_tab_player = (Indice_tab_player + 1)%2;
+            }
+            if (played) {
+                swap(plateau, ligne, column, Indice_tab_player, Possible_vect);
+            }
+            else printf("Coup invalide \n");
+
+            Print_tab(plateau);
+            end_game = 0;
+        }
+        if (played) Indice_tab_player = (Indice_tab_player + 1)%2;
+
+    }
     return 0;
 }
