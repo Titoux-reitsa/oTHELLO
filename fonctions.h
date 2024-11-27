f62c4800e6cd480b644f523f8370a54e32ee00f0
Titoux-reitsa <mertrier5555@gmail.com>
2024-11-27 16:54:50 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -1,13 +1,22 @@
 #include <stdbool.h>
 #define Lenght_tab 8
 
+#define RESET "\x1b[0m"
+#define BLACK_BG "\x1b[48;5;22m"  // Fond vert foncé
+#define WHITE_BG "\x1b[48;5;28m"  // Fond vert clair
+
 
 // Fonctions de fonctions_bases.c
 
 void Init_tab(int plateau[Lenght_tab][Lenght_tab]);
 void Print_tab(int plateau[Lenght_tab][Lenght_tab]); 
 bool Is_empty(int plateau[Lenght_tab][Lenght_tab], int ligne, int column); 
-bool Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]);
+bool Tab_is_empty(int plateau[Lenght_tab][Lenght_tab], int Indice_player, int Possible_vect[8][2]);
+int Is_possible(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]);
 void swap(int plateau[Lenght_tab][Lenght_tab], int i, int j, int Indice_player, int Possible_vect[8][2]);
+int Who_win(int plateau[Lenght_tab][Lenght_tab]);
 
 // Fonctions de IA.c
+int IA(int plateau[Lenght_tab][Lenght_tab], int Indice_player);
+
+// Fonctions de test.c
